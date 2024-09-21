4c2a7aa8be6351c1e78f7d40db8659328f16fdc8
zhe28 <huangze28@foxmail.com>
2024-09-21 10:35:50 +0800

    fix[fe]: 移除过时的 prop 引用

--- fe/src/components/HomeAside.vue
+++ fe/src/components/HomeAside.vue
@@ -1,48 +1,49 @@
 <template>
   <div id="main">
-    <input id="search" :placeholder="lang.search">
-    <el-tree :data="data" :props="defaultProps" :defaultExpandAll="true" @node-click="handleNodeClick"/>
+    <input id="search" :placeholder="lang.search" />
+    <el-tree
+      :data="data"
+      :defaultExpandAll="true"
+      @node-click="handleNodeClick"
+    />
   </div>
 </template>
 
-
 <script setup>
-import {useRouter} from 'vue-router'
-import {ref} from 'vue'
-import useGroupStore from '../stores/group'
-import lang from '../i18n/i18n';
-import {http} from "@/utils/axios";
-import {defaultProps} from "element-plus/es/components/select-v2/src/useProps";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+import useGroupStore from "../stores/group";
+import lang from "../i18n/i18n";
+import { http } from "@/utils/axios";
 
-const groupStore = useGroupStore()
-const router = useRouter()
-const data = ref([])
+const groupStore = useGroupStore();
+const router = useRouter();
+const data = ref([]);
 
-http.get('/api/group').then(res => {
-  if (res.data) data.value = res.data
-})
+http.get("/api/group").then((res) => {
+  if (res.data) data.value = res.data;
+});
 
 const handleNodeClick = function (data) {
   if (data.tag != null) {
-    groupStore.name = data.label
-    groupStore.tag = data.tag
+    groupStore.name = data.label;
+    groupStore.tag = data.tag;
     router.push({
       name: "list",
-    })
+    });
   }
-}
+};
 </script>
-
 
 <style scoped>
 #main {
   width: 243px;
-  background-color: #F1F1F1;
+  background-color: #f1f1f1;
   height: 100%;
 }
 
 #search {
-  background-color: #D6E7F7;
+  background-color: #d6e7f7;
   width: 100%;
   height: 40px;
   padding-left: 10px;
@@ -52,7 +53,7 @@ const handleNodeClick = function (data) {
 }
 
 .el-tree {
-  background-color: #F1F1F1;
+  background-color: #f1f1f1;
 }
 
 .add_group {
