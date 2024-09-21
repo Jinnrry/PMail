1fffa1d89ba6268498ff04bb52b020e83123e0c9
zhe28 <huangze28@foxmail.com>
2024-09-21 10:35:50 +0800

    fix[fe]: 移除过时的 prop 引用

--- fe/src/components/GroupSettings.vue
+++ fe/src/components/GroupSettings.vue
@@ -1,13 +1,36 @@
 <template>
   <div id="main">
-    <el-tree :expand-on-click-node="false" :data="data" :props="defaultProps" :defaultExpandAll="true">
+    <el-tree
+      :expand-on-click-node="false"
+      :data="data"
+      :defaultExpandAll="true"
+    >
       <template #default="{ node, data }">
         <div>
           <span v-if="data.id !== -1"> {{ data.label }}</span>
-          <el-input v-if="data.id === -1" v-model="data.label" @blur="onInputBlur(data)"></el-input>
-          <el-button v-if="data.id !== 0" @click="del(node, data)" size="small" type="danger" circle> -
+          <el-input
+            v-if="data.id === -1"
+            v-model="data.label"
+            @blur="onInputBlur(data)"
+          ></el-input>
+          <el-button
+            v-if="data.id !== 0"
+            @click="del(node, data)"
+            size="small"
+            type="danger"
+            circle
+          >
+            -
           </el-button>
-          <el-button v-if="data.id !== 0" @click="add(data)" size="small" type="primary" circle> +</el-button>
+          <el-button
+            v-if="data.id !== 0"
+            @click="add(data)"
+            size="small"
+            type="primary"
+            circle
+          >
+            +
+          </el-button>
         </div>
       </template>
     </el-tree>
@@ -17,83 +40,84 @@
 </template>
 
 <script setup>
-import {reactive} from 'vue'
-import lang from '../i18n/i18n';
-import {http} from "@/utils/axios";
-import {ElMessage} from "element-plus";
-import {defaultProps} from "element-plus/es/components/select-v2/src/useProps";
+import { reactive } from "vue";
+import lang from "../i18n/i18n";
+import { http } from "@/utils/axios";
+import { ElMessage } from "element-plus";
 
-const data = reactive([])
+const data = reactive([]);
 
-http.get('/api/group').then(res => {
-  data.push(...res.data)
-})
+http.get("/api/group").then((res) => {
+  data.push(...res.data);
+});
 
 const del = function (node, data) {
   if (data.id !== -1) {
-    this.$axios.post("/api/group/del", {"id": data.id}).then(res => {
+    this.$axios.post("/api/group/del", { id: data.id }).then((res) => {
       if (res.errorNo !== 0) {
         ElMessage({
           message: res.errorMsg,
-          type: 'error',
-        })
+          type: "error",
+        });
       } else {
-        const pc = node.parent.childNodes
+        const pc = node.parent.childNodes;
         for (let i = 0; i < pc.length; i++) {
           if (pc[i].id === node.id) {
-            pc.splice(i, 1)
-            return
+            pc.splice(i, 1);
+            return;
           }
         }
       }
-    })
+    });
   } else {
-    const pc = node.parent.childNodes
+    const pc = node.parent.childNodes;
     for (let i = 0; i < pc.length; i++) {
       if (pc[i].id === node.id) {
-        pc.splice(i, 1)
-        return
+        pc.splice(i, 1);
+        return;
       }
     }
   }
-}
+};
 
 const add = function (item) {
   if (item.children == null) {
-    item.children = []
+    item.children = [];
   }
   item.children.push({
-    "children": [],
-    "label": "",
-    "id": "-1",
-    "parent_id": item.id
-  })
-}
+    children: [],
+    label: "",
+    id: "-1",
+    parent_id: item.id,
+  });
+};
 
 const addRoot = function () {
   data.push({
-    "children": [],
-    "label": "",
-    "id": "-1",
-    "parent_id": 0
-  })
-}
+    children: [],
+    label: "",
+    id: "-1",
+    parent_id: 0,
+  });
+};
 
 const onInputBlur = function (item) {
   if (item.label !== "") {
-    http.post("/api/group/add", {"name": item.label, "parent_id": item.parent_id}).then(res => {
-      if (res.errorNo !== 0) {
-        ElMessage({
-          message: res.errorMsg,
-          type: 'error',
-        })
-      } else {
-        this.$axios.get('/api/group').then(res => {
-          data.splice(0, data.length)
-          data.push(...res.data)
-        })
-      }
-    })
+    http
+      .post("/api/group/add", { name: item.label, parent_id: item.parent_id })
+      .then((res) => {
+        if (res.errorNo !== 0) {
+          ElMessage({
+            message: res.errorMsg,
+            type: "error",
+          });
+        } else {
+          this.$axios.get("/api/group").then((res) => {
+            data.splice(0, data.length);
+            data.push(...res.data);
+          });
+        }
+      });
   }
-}
+};
 </script>
